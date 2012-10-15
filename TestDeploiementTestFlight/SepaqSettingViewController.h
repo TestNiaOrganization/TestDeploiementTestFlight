a52c7eca62a71c22f03de49fb900b896ae784000
NiaSepaq <dang.nghia@sepaq.com>
2012-10-15 11:08:50 -0400

    Ajout du SettingViewController
    Ajout de 3 nouvelles classes pour le SettingViewController dans le
    projet.

---
+++ TestDeploiementTestFlight/SepaqSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SepaqSettingViewController.h
+//  TestDeploiementTestFlight
+//
+//  Created by N Dang on 10/15/12.
+//  Copyright (c) 2012 com.sepaq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SepaqSettingViewController : UITableViewController
+
+@end
